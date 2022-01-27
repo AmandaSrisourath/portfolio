fa53f1f57f5f0d9faf18eeb0e5366973e45d0d1e
Amandozz <amanda.srisou@gmail.com>
2022-01-27 21:07:31 +1000

    Add React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "@emotion/styled";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Container>
+          <AboutMe />
+          <Projects />
+      </Container>
+      <Footer />
     </div>
-  );
+  )
 }
+
+const Container = styled.div`
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
 
 export default App;
