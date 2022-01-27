ec3ef81e2dfd130255483066c91b94c9c5b16a60
Amandozz <amanda.srisou@gmail.com>
2022-01-27 21:08:36 +1000

    Create AboutMe component

---
+++ src/components/AboutMe.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function AboutMe() {
+    return (
+        <div>
+            <h2>
+                About me
+            </h2>
+
+            <p>
+                After a carrer reorientation from sales management to web design and front-end development, I am looking for a company to grow in this field.
+            </p>
+        </div>
+    )
+}
+
+export default AboutMe;
