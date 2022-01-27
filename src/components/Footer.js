7dff591f2818f1b590d61f960e772a9f75f10806
Amandozz <amanda.srisou@gmail.com>
2022-01-27 21:09:26 +1000

    Create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from '../logo.svg';
+
+function Footer() {
+    return (
+        <div>
+            {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        </div>
+    )
+}
+
+export default Footer;
