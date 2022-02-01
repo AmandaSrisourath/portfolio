4a82997196e9ebbcd36f0869c3c706853c56c2e0
Amandozz <amanda.srisou@gmail.com>
2022-02-01 13:43:19 +1000

    Create AboutMe component

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const projects = [
+    {
+        title: 'Convert a jQuery library to React',
+        description: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
+        technologies: ['React', 'React Router', 'Redux / Toolkit', 'Emotion', 'Jest', 'Testing-Library'],
+    },
+    {
+        title: 'Use an API for a bank user account with React',
+        description: ['Implement a state manager in a React application using Redux / Toolkit', 'Interact with an API', 'Authentication with an API'],
+        technologies: ['React', 'React Router', 'Redux / Toolkit', 'Axios'],
+    },
+    {
+        title: 'Build an analytics dashboard with React',
+        description: ['Interact with a web service', 'Develop advanced graphical elements using recharts', 'Produce technical documentation for an application'],
+        technologies: ['React', 'React Router', 'Axios', 'Recharts'],
+    },
+    {
+        title: 'Create an application with React and React Router',
+        description: ['Developing routes for a web application with React Router', 'Initialize a web application with a framework', 'Create components with React'],
+        technologies: ['React', 'React Router'],
+    },
+    {
+        title: 'Convert a jQuery library to React',
+        description: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
+        technologies: ['React', 'React Router', 'Redux / Toolkit', 'Emotion', 'Jest', 'Testing-Library'],
+    },
+    {
+        title: 'Convert a jQuery library to React',
+        description: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
+        technologies: ['React', 'React Router', 'Redux / Toolkit', 'Emotion', 'Jest', 'Testing-Library'],
+    },
+    {
+        title: 'Convert a jQuery library to React',
+        description: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
+        technologies: ['React', 'React Router', 'Redux / Toolkit', 'Emotion', 'Jest', 'Testing-Library'],
+    },
+    {
+        title: 'Convert a jQuery library to React',
+        description: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
+        technologies: ['React', 'React Router', 'Redux / Toolkit', 'Emotion', 'Jest', 'Testing-Library'],
+    },
+]
+
+export { projects };
