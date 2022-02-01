170368f4a1fc9f635873852fcf8483789850e694
Amandozz <amanda.srisou@gmail.com>
2022-02-01 13:49:02 +1000

    Create data

--- src/data.js
+++ src/data.js
@@ -1,44 +1,52 @@
-const projects = [
+const data = [
     {
+        id: '1',
         title: 'Convert a jQuery library to React',
-        description: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
+        skills: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
         technologies: ['React', 'React Router', 'Redux / Toolkit', 'Emotion', 'Jest', 'Testing-Library'],
     },
     {
+        id: '2',
         title: 'Use an API for a bank user account with React',
-        description: ['Implement a state manager in a React application using Redux / Toolkit', 'Interact with an API', 'Authentication with an API'],
+        skills: ['Implement a state manager in a React application using Redux / Toolkit', 'Interact with an API', 'Authentication with an API'],
         technologies: ['React', 'React Router', 'Redux / Toolkit', 'Axios'],
     },
     {
+        id: '3',
         title: 'Build an analytics dashboard with React',
-        description: ['Interact with a web service', 'Develop advanced graphical elements using recharts', 'Produce technical documentation for an application'],
+        skills: ['Interact with a web service', 'Develop advanced graphical elements using recharts', 'Produce technical documentation for an application'],
         technologies: ['React', 'React Router', 'Axios', 'Recharts'],
     },
     {
+        id: '4',
         title: 'Create an application with React and React Router',
-        description: ['Developing routes for a web application with React Router', 'Initialize a web application with a framework', 'Create components with React'],
+        skills: ['Developing routes for a web application with React Router', 'Initialize a web application with a framework', 'Create components with React'],
         technologies: ['React', 'React Router'],
     },
     {
+        id: '5',
         title: 'Convert a jQuery library to React',
-        description: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
+        skills: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
         technologies: ['React', 'React Router', 'Redux / Toolkit', 'Emotion', 'Jest', 'Testing-Library'],
     },
     {
+        id: '6',
         title: 'Convert a jQuery library to React',
-        description: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
+        skills: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
         technologies: ['React', 'React Router', 'Redux / Toolkit', 'Emotion', 'Jest', 'Testing-Library'],
     },
     {
+        id: '7',
         title: 'Convert a jQuery library to React',
-        description: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
+        skills: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
         technologies: ['React', 'React Router', 'Redux / Toolkit', 'Emotion', 'Jest', 'Testing-Library'],
     },
     {
+        id: '8',
         title: 'Convert a jQuery library to React',
-        description: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
+        skills: ['Redesign an application to reduce technical debt', 'Analyze the performance of a web application', 'Deploy a front-end application', 'Unit testing'],
         technologies: ['React', 'React Router', 'Redux / Toolkit', 'Emotion', 'Jest', 'Testing-Library'],
     },
 ]
 
-export { projects };
+export { data };
