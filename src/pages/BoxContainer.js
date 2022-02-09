237c15b1e2b3a0d43db9da38b9c218ad1b0956de
Amandozz <amanda.srisou@gmail.com>
2022-02-09 12:37:51 +1000

    Change background color

--- src/pages/BoxContainer.js
+++ src/pages/BoxContainer.js
@@ -3,27 +3,24 @@ import Box from '@mui/material/Box';
 
 function BoxContainer(props) {    
     return (
-        <div>
-            <Box
-                sx={{
-                    width: 300,
-                    height: 300,
-                    backgroundColor: '#20232a',
-                    borderRadius: 4,
-                    margin: 3,
-                    padding: 4,
-                    overflow: 'auto',
-                    color: 'white',
-                    '&:hover': {
-                        backgroundColor: '#61dafb',
-                        opacity: [0.9, 0.8, 0.7],
-                        color: 'black',
-                    },
-                }}
-            >
-                {props.children}
-            </Box>
-        </div>
+        <Box
+            sx={{
+                width: 300,
+                height: 300,
+                backgroundColor: 'white',
+                opacity: [0.9, 0.8, 0.7],
+                borderRadius: 2,
+                margin: 3,
+                padding: 4,
+                overflow: 'auto',
+                '&:hover': {
+                    backgroundColor: 'white',
+                    opacity: 1,
+                },
+            }}
+        >
+            {props.children}
+        </Box>
     )
 }
 
