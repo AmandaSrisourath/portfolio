3f274df99dbf6a82e0b3bb3466dd38e30e203eab
Amandozz <amanda.srisou@gmail.com>
2022-02-11 13:53:05 +1000

    Update path

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ import {
 	Route,
   } from "react-router-dom";
 import Home from './pages/home/index';
-import AboutMe from './pages/Infos/AboutMe';
+import Infos from './pages/infos/index';
 import Projects from './pages/projects/components/index';
-import Infos from './pages/Infos/index';
-import SkillsAndSoftwares from './pages/skills & softwares/index';
+import SkillsAndSoftwares from './pages/skills-softwares/index';
 
 function App() {
   	return (
     	<Router>
 			<Routes>
 				<Route path="/" element={<Home/>}/>
-				<Route path="/aboutMe" element={<AboutMe/>}/>
 				<Route path="/infos" element={<Infos/>}/>
 				<Route path="/projects" element={<Projects/>}/>
 				<Route path="/skills-softwares" element={<SkillsAndSoftwares/>}/>
