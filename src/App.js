a9f78366260e7914ce7d346ec0111923df344578
Amandozz <amanda.srisou@gmail.com>
2022-02-21 20:22:50 +1000

    Add google analytics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import {
 	Route,
   } from "react-router-dom";
 import Home from './pages/home';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "G-F4GZFE8HJ8";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   	return (
