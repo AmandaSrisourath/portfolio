a1374acdb961fdf6e1acccb8f9fe0f005ef7e161
Amandozz <amanda.srisou@gmail.com>
2022-02-23 10:30:54 +1000

    Add component Footer

---
+++ src/pages/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { styled } from '@mui/system';
+import SocialLinks from "../../src/pages/infos/SocialLinks";
+
+function Footer() {
+    return (
+        <Container>
+            <SocialLinks/>
+        </Container>
+    )
+}
+
+const Container = styled('div')`
+    background-color: white;
+    opacity: 0.9;
+    display: flex;
+    justify-content: center;
+    padding: 32px;
+    margin-top: 32px
+`
+
+export default Footer;
