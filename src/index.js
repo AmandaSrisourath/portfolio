f8fd7c42c570a62ce99f397bfb1fe972c5f64fbf
Amanda Srisourath <amanda.srisourath@powertec.com.au>
2024-03-11 15:57:13 +1000

    Fix some issues

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA65Y3WkjbRTgFmcWw9L0sztPjstmkGAlU",
@@ -13,17 +13,17 @@ const firebaseConfig = {
   storageBucket: "portfolio-ea231.appspot.com",
   messagingSenderId: "481733143966",
   appId: "1:481733143966:web:52105872672376b7d0fa8f",
-  measurementId: "G-TFXVMXQ4H6"
+  measurementId: "G-TFXVMXQ4H6",
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Infos/SocialLinks.js
+++ src/pages/Infos/SocialLinks.js
@@ -1,34 +1,70 @@
+import { GitHub, LinkedIn, Twitter } from "@mui/icons-material";
+import { styled } from "@mui/system";
 import React from "react";
-import { styled } from '@mui/system';
-import { GitHub, LinkedIn, Twitter } from '@mui/icons-material';
 
 function SocialLinks() {
-    let urlGitHub = "https://github.com/AmandaSrisourath";
-    let urlLinkedin= "https://www.linkedin.com/in/a-srisourath/";
-    let urlTwitter= "https://twitter.com/AmdaSrisourath";
+  let urlGitHub = "https://github.com/AmandaSrisourath";
+  let urlLinkedin = "https://www.linkedin.com/in/a-srisourath/";
+  let urlTwitter = "https://twitter.com/AmdaSrisourath";
 
-    return (
-        <div>
-            <Links>
-                <a href={urlGitHub} alt="GitHub logo" aria-label="GitHub logo" target="_blank">
-                    <GitHub sx={{ fontSize: 30, color: 'black', '&:hover': { color: '#61dafb'} }}/>
-                </a>
-                <a href={urlLinkedin} alt="LinkedIn logo" aria-label="LinkedIn logo" target="_blank">
-                    <LinkedIn sx={{ fontSize: 37, color: 'black', '&:hover': { color: '#61dafb'} }}/>
-                </a>
-                <a href={urlTwitter} alt="Twitter logo" aria-label="Twitter logo" target="_blank">
-                    <Twitter sx={{ fontSize: 37, color: 'black', '&:hover': { color: '#61dafb'} }}/>
-                </a>
-            </Links>
-        </div>
-    )
+  return (
+    <div>
+      <Links>
+        <a
+          href={urlGitHub}
+          alt="GitHub logo"
+          aria-label="GitHub logo"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GitHub
+            sx={{
+              fontSize: 30,
+              color: "black",
+              "&:hover": { color: "#61dafb" },
+            }}
+          />
+        </a>
+        <a
+          href={urlLinkedin}
+          alt="LinkedIn logo"
+          aria-label="LinkedIn logo"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedIn
+            sx={{
+              fontSize: 37,
+              color: "black",
+              "&:hover": { color: "#61dafb" },
+            }}
+          />
+        </a>
+        <a
+          href={urlTwitter}
+          alt="Twitter logo"
+          aria-label="Twitter logo"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Twitter
+            sx={{
+              fontSize: 37,
+              color: "black",
+              "&:hover": { color: "#61dafb" },
+            }}
+          />
+        </a>
+      </Links>
+    </div>
+  );
 }
 
-const Links = styled('div')`
-    width: 150px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`
+const Links = styled("div")`
+  width: 150px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export default SocialLinks;
