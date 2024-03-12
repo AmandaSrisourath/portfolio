3e93da4f814f579567b6f934885acd578a555b81
Amanda Srisourath <amanda.srisourath@powertec.com.au>
2024-03-12 14:24:03 +1000

    Increase border radius of box container

--- src/pages/BoxContainer.js
+++ src/pages/BoxContainer.js
@@ -1,26 +1,26 @@
+import Box from "@mui/material/Box";
 import React from "react";
-import Box from '@mui/material/Box';
 
-function BoxContainer(props) {    
-    return (
-        <Box
-            sx={{
-                width: 300,
-                height: 300,
-                backgroundColor: 'white',
-                opacity: 0.9,
-                borderRadius: 1,
-                margin: 1,
-                padding: 3,
-                '&:hover': {
-                    backgroundColor: 'white',
-                    opacity: 1,
-                },
-            }}
-        >
-            {props.children}
-        </Box>
-    )
+function BoxContainer(props) {
+  return (
+    <Box
+      sx={{
+        width: 300,
+        height: 300,
+        backgroundColor: "white",
+        opacity: 0.9,
+        borderRadius: 2,
+        margin: 1,
+        padding: 3,
+        "&:hover": {
+          backgroundColor: "white",
+          opacity: 1,
+        },
+      }}
+    >
+      {props.children}
+    </Box>
+  );
 }
 
 export default BoxContainer;
